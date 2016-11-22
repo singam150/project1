1c4bd9f11f23e2047e5787618cc9e23c6050d0e6
Srinivas Reddy <srinivas.singam1255@gmail.com>
2016-11-22 13:07:55 +0530

    added app1.js

---
+++ JS/app1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Finger print data indexing srinu</title>
+  </head>
+  <body>
+    <h1> welcome to raudra technologies</h1>
+
+  </body>
+</html>
